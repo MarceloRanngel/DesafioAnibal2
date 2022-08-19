35baa0e4c74552cabc4a62941c8d19e085af0a63
Rangel <marcelo.rcs@icloud.com>
2022-08-19 17:24:36 -0300

    Adicionar arquivos de projeto.

---
+++ Prova/Pages/Index.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+
+namespace Prova.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public FraseInfo fraseInfo = new FraseInfo();
+        public String statusMessage = "";
+        private readonly ILogger<IndexModel> _logger;
+        public IndexModel(ILogger<IndexModel> logger)
+
+                
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            fraseInfo.frase = Request.Form["frase"];
+
+            try
+            {
+                String connectionString = "Data Source=DESKTOP-LIMIU14;Initial Catalog=ProvaTecnica;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {   
+                    connection.Open();
+                    String sql = "INSERT INTO frase " +
+                        "(frase) VALUES" +
+                        "(@frase)";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@frase", fraseInfo.frase);
+                        command.ExecuteNonQuery();
+                    }
+
+                    statusMessage = "Enviado com sucesso!";
+                }
+            }
+            catch (Exception ex)
+            {   
+                statusMessage = ex.Message;
+                Console.Write(ex.ToString());
+            }
+
+            
+        }
+
+        public class FraseInfo
+        {
+            public string id;
+            public string frase;
+        }
+    }
+}
